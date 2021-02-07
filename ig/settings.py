8334cee683d12b44dc3fa80072c7f70ddc9356da
Madalitso Muva <madalitsomuva@gmail.com>
2021-02-07 11:01:13 +0200

    pereniel

--- ig/settings.py
+++ ig/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '0@mx_t7v40^326i-@^#*tqe4=hgi2^tr(=nx85#iwf1ju)q)7)'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['v2kaj.pythonanywhere.com',]
+ALLOWED_HOSTS = ['v2kaj.pythonanywhere.com',  '127.0.0.1',]
 
 
 # Application definition
--- savings/views.py
+++ savings/views.py
@@ -8,32 +8,5 @@ from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def index(request):
-    if not request.user.is_authenticated:
-        return render(request, "savings/index.html")
-    else:
-        return HttpResponseRedirect(reverse("account"))
-
-@login_required
-def account_view(request):
-    deposits = Deposit.objects.all().filter(deposited_by=request.user).order_by('-deposit_date')
-    return render(request, "savings/account.html", {"deposits": deposits})
-
-def login_view(request):
-    if request.method == "POST":
-        # Attempt to sign user in
-        username = request.POST["username"]
-        password = request.POST["password"]
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            return HttpResponseRedirect(reverse("account"))
-        else:
-            return render(request, "savings/index.html", {
-                "message": "Invalid username and/or password."
-            })
-    else:
-        return render(request, "savings/index.html")
-
-def logout_view(request):
-    logout(request)
-    return HttpResponseRedirect(reverse("index"))
+    return render(request, "savings/index.html")
+   
